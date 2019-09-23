32554ed982cdd9b09f78c68dfea47ba496476dd0
justinjkim <justinjkim@cb5109-12.labs.sce.carleton.ca>
2019-09-23 11:42:53 -0400

    first

---
+++ src/BuddyInfo.java
@@ -0,0 +1,49 @@
+
+public class BuddyInfo {
+	
+	private String name;
+	private String address;
+	private String phoneNumber;
+	
+	public BuddyInfo() {
+		
+	}
+	
+	public BuddyInfo(String name, String address, String phone) {
+		this.name = name;
+		this.address = address;
+		this.phoneNumber = phone;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public static void main(String[] args) {
+		BuddyInfo buddy = new BuddyInfo();
+		buddy.setName("Justin");
+		System.out.println("Hello " + buddy.getName());
+
+	}
+
+}
